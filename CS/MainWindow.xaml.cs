56bc7630c919385c5a1bd555a16cf0ba32f68dd6
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DevExpress.Xpf.Grid;
+using System.Collections.ObjectModel;
+using DevExpress.Data.Filtering.Helpers;
+using System.ComponentModel;
+
+namespace DXSample {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+            List<TestData> list = new List<TestData>();
+            for(int i = 0; i < 20; i++) {
+                list.Add(new TestData(i) { ParentId = 2 });
+            }
+            list[4].ParentId = -1;
+            list[5].ParentId = 4;
+            list[6].ParentId = 4;
+            list[7].ParentId = 4;
+            treeList.ItemsSource = list;
+            Loaded += new RoutedEventHandler(MainWindow_Loaded);
+        }
+
+        void MainWindow_Loaded(object sender, RoutedEventArgs e) {
+            CleanNodesList();
+        }
+
+        bool isFirst = true;
+        List<TreeListNode> nodesList = new List<TreeListNode>();
+        private void TreeListView_CustomNodeFilter(object sender, DevExpress.Xpf.Grid.TreeList.TreeListNodeFilterEventArgs e) {
+            TreeListView view = sender as TreeListView;
+            if(isFirst) {
+                ExpressionEvaluator ee = new ExpressionEvaluator(TypeDescriptor.GetProperties(typeof(TestData)), view.DataControl.FilterCriteria);
+                foreach(TreeListNode node in new TreeListNodeIterator(view.Nodes)) {
+                    if(ee.Fit(node.Content))
+                        nodesList.Add(node);
+                }
+                isFirst = false;
+            }
+            if(e.Node.HasChildren) {
+                foreach(TreeListNode node in nodesList) {
+                    if(node.IsDescendantOf(e.Node)) {
+                        e.Visible = true;
+                        e.Handled = true;
+                        return;
+                    }
+                }
+            }
+        }
+
+        private void treeList_FilterChanged(object sender, RoutedEventArgs e) {
+            CleanNodesList();
+        }
+        
+        void CleanNodesList() {
+            isFirst = true;
+            nodesList.Clear();
+        }
+    }
+
+    public class TestData {
+        public int KeyId { get; set; }
+        public int ParentId { get; set; }
+        public int Number { get; set; }
+        public string Text { get; set; }
+        public bool Bool { get; set; }
+        public TestData(int i) {
+            Number = i;
+            KeyId = i;
+            Text = "Row" + i;
+            Bool = i % 3 != 0;
+        }
+    }
+}
---
+++ VB/MainWindow.xaml.vb
@@ -0,0 +1,131 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Text
+Imports System.Windows
+Imports System.Windows.Controls
+Imports System.Windows.Data
+Imports System.Windows.Documents
+Imports System.Windows.Input
+Imports System.Windows.Media
+Imports System.Windows.Media.Imaging
+Imports System.Windows.Navigation
+Imports System.Windows.Shapes
+Imports DevExpress.Xpf.Grid
+Imports System.Collections.ObjectModel
+Imports DevExpress.Data.Filtering.Helpers
+Imports System.ComponentModel
+
+Namespace DXSample
+	''' <summary>
+	''' Interaction logic for MainWindow.xaml
+	''' </summary>
+	Partial Public Class MainWindow
+		Inherits Window
+		Public Sub New()
+			InitializeComponent()
+			Dim list As New List(Of TestData)()
+			For i As Integer = 0 To 19
+				list.Add(New TestData(i) With {.ParentId = 2})
+			Next i
+			list(4).ParentId = -1
+			list(5).ParentId = 4
+			list(6).ParentId = 4
+			list(7).ParentId = 4
+			treeList.ItemsSource = list
+			AddHandler Loaded, AddressOf MainWindow_Loaded
+		End Sub
+
+		Private Sub MainWindow_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			CleanNodesList()
+		End Sub
+
+		Private isFirst As Boolean = True
+		Private nodesList As New List(Of TreeListNode)()
+		Private Sub TreeListView_CustomNodeFilter(ByVal sender As Object, ByVal e As DevExpress.Xpf.Grid.TreeList.TreeListNodeFilterEventArgs)
+			Dim view As TreeListView = TryCast(sender, TreeListView)
+			If isFirst Then
+				Dim ee As New ExpressionEvaluator(TypeDescriptor.GetProperties(GetType(TestData)), view.DataControl.FilterCriteria)
+				For Each node As TreeListNode In New TreeListNodeIterator(view.Nodes)
+					If ee.Fit(node.Content) Then
+						nodesList.Add(node)
+					End If
+				Next node
+				isFirst = False
+			End If
+			If e.Node.HasChildren Then
+				For Each node As TreeListNode In nodesList
+					If node.IsDescendantOf(e.Node) Then
+						e.Visible = True
+						e.Handled = True
+						Return
+					End If
+				Next node
+			End If
+		End Sub
+
+		Private Sub treeList_FilterChanged(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			CleanNodesList()
+		End Sub
+
+		Private Sub CleanNodesList()
+			isFirst = True
+			nodesList.Clear()
+		End Sub
+	End Class
+
+	Public Class TestData
+		Private privateKeyId As Integer
+		Public Property KeyId() As Integer
+			Get
+				Return privateKeyId
+			End Get
+			Set(ByVal value As Integer)
+				privateKeyId = value
+			End Set
+		End Property
+		Private privateParentId As Integer
+		Public Property ParentId() As Integer
+			Get
+				Return privateParentId
+			End Get
+			Set(ByVal value As Integer)
+				privateParentId = value
+			End Set
+		End Property
+		Private privateNumber As Integer
+		Public Property Number() As Integer
+			Get
+				Return privateNumber
+			End Get
+			Set(ByVal value As Integer)
+				privateNumber = value
+			End Set
+		End Property
+		Private privateText As String
+		Public Property Text() As String
+			Get
+				Return privateText
+			End Get
+			Set(ByVal value As String)
+				privateText = value
+			End Set
+		End Property
+		Private privateBool As Boolean
+		Public Property Bool() As Boolean
+			Get
+				Return privateBool
+			End Get
+			Set(ByVal value As Boolean)
+				privateBool = value
+			End Set
+		End Property
+		Public Sub New(ByVal i As Integer)
+			Number = i
+			KeyId = i
+			Text = "Row" & i
+			Bool = i Mod 3 <> 0
+		End Sub
+	End Class
+End Namespace
